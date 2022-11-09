39da7f0f2244377882ba7a40e8ed86a81e2b8e0f
Arghya20 <arghyaghosh.vlog@gmail.com>
2022-11-10 02:35:22 +0530

    basic req complited

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Our Server is running 🤘");
+});
+app.listen(port, () => {
+  console.log(`Our app running on port ${port}`);
+});
