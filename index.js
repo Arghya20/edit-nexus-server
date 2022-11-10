3a38e4cae0372c6e5188cb23261ae81813a1ada0
Arghya20 <arghyaghosh.vlog@gmail.com>
2022-11-10 19:59:08 +0530

    data lodeded from mongo db

--- index.js
+++ index.js
@@ -16,6 +16,25 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+      try{
+        const serviceCollection = client.db('editNexus').collection('services');
+
+        app.get('/services', async (req, res) => {
+          const query = {}
+          const cursor = serviceCollection.find(query);
+          const services = await cursor.toArray();
+          res.send(services);
+      });
+
+      }
+      finally{
+
+      }
+}
+run().catch(console.dir);
+
+
 
 app.get("/", (req, res) => {
   res.send("Our Server is running 🤘");
