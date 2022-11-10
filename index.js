91d5b143119907ae508cfb1b48bdea4a4be7f78f
Arghya20 <arghyaghosh.vlog@gmail.com>
2022-11-10 19:36:56 +0530

    mongo env setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 
 const app = express();
@@ -8,6 +9,13 @@ const port = process.env.PORT || 5000;
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.s14y4qr.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get("/", (req, res) => {
   res.send("Our Server is running 🤘");
